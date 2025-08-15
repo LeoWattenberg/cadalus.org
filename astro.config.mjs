83d73b54f416a1acf6683bb1b1d585df353294c6
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2025-08-15 13:34:32 +0200

    vibelation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import i18next from './src/i18n.ts';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [i18next],
+});
